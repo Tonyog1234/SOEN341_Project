ee9a35153871fa5e9de995ee13795a9d154abce3
Tony <tony0953112356@gmail.com>
2025-02-05 23:55:30 -0500

    apply style

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,10 +2,14 @@ import React, { useState } from "react";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../config/firebase";
 import { useNavigate } from "react-router-dom";
+import "./Style.css";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [RegisterEmail, setRegisterEmail] = useState("");
+  const [RegisterPassword, setRegisterPassword] = useState("");
+  const [role, setRole] = useState("");
   const navigate = useNavigate();
 
   const handleLogin = async (e) => {
@@ -17,25 +21,94 @@ const Login = () => {
       alert(error.message);
     }
   };
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password, role);
+      navigate("/dashboard"); // Redirect to dashboard after login
+    } catch (error) {
+      alert(error.message);
+    }
+  };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleLogin}>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Login</button>
-      </form>
+    <div className="OuterContainer">
+      <div className="FormContainer">
+        <div>
+          <h1 className="Login">Login</h1>
+          <form onSubmit={handleLogin} className="FormSubmit">
+            <div>
+              <label className="Email">Email</label>
+              <input
+                className="InputEmail"
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label className="Password">Password</label>
+              <input
+                className="InputPassword"
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+            <button type="submit" className="LoginButton">
+              Login
+            </button>
+          </form>
+          <hr className="my-4" />
+
+          <h1 className="Register">Register</h1>
+          <form onSubmit={handleRegister} className="FormRegister">
+            <div>
+              <label className="Email">Email</label>
+              <input
+                className="InputEmail"
+                type="email"
+                placeholder="Email"
+                value={RegisterEmail}
+                onChange={(e) => setRegisterEmail(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label className="Password">Password</label>
+              <input
+                className="InputPassword"
+                type="password"
+                placeholder="Password"
+                value={RegisterPassword}
+                onChange={(e) => setRegisterPassword(e.target.value)}
+                required
+              />
+            </div>
+            <div>
+              <label className="Role">Role</label>
+              <select
+                className="RoleSelect"
+                // w-full p-2 border border-gray-300 rounded-lg
+                value={role}
+                onChange={(e) => setRole(e.target.value)}
+                required
+              >
+                <option value="">Select a role</option>
+                <option value="admin">Admin</option>
+                <option value="user">User</option>
+              </select>
+            </div>
+            <button type="submit" className="RegisterButton">
+              Register
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };

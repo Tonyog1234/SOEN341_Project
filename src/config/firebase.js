6ed02d0d15f9468922f057a20cb1828e7a07e34d
Tony <tony0953112356@gmail.com>
2025-02-06 22:51:24 -0500

    update firestore

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-
+import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
   apiKey: "AIzaSyCOpVLxfmJ5wG4BPHj_JWjlGZj4dOfkum8",
   authDomain: "soen341-5e47f.firebaseapp.com",
@@ -9,11 +9,12 @@ const firebaseConfig = {
   storageBucket: "soen341-5e47f.firebasestorage.app",
   messagingSenderId: "646434903420",
   appId: "1:646434903420:web:8f554309476d629f14c0f9",
-  measurementId: "G-TCZE9WQ4CD"
+  measurementId: "G-TCZE9WQ4CD",
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app); // Initialize Firestore
 
-export { auth };
+export { auth, db };
